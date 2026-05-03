8ca17289ce52a791ec686efb3d018450faa044dd
Towaris4 <ivan.tarasov2488@gmail.com>
2026-05-03 06:48:22 +0300

    4. Многопользовательские приложения. Многопоточность в Веб приложении. [#504857]

--- src/main/java/ru/job4j/dreamjob/controller/VacancyController.java
+++ src/main/java/ru/job4j/dreamjob/controller/VacancyController.java
@@ -1,5 +1,7 @@
 package ru.job4j.dreamjob.controller;
 
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -9,6 +11,7 @@ import ru.job4j.dreamjob.service.VacancyService;
 
 @Controller
 @RequestMapping("/vacancies")
+@ThreadSafe
 public class VacancyController {
 
     private final VacancyService vacancyService;
