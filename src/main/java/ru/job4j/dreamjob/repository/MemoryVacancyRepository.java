03fff459da4f2a6ed602acda1258d177c4c4d358
Towaris4 <ivan.tarasov2488@gmail.com>
2026-05-04 19:47:27 +0300

    2. CandidateRepository. Хранение кандидатов. [#504861]

--- src/main/java/ru/job4j/dreamjob/repository/MemoryVacancyRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryVacancyRepository.java
@@ -16,7 +16,6 @@ import java.util.concurrent.atomic.AtomicInteger;
 @ThreadSafe
 public class MemoryVacancyRepository implements VacancyRepository {
 
-
     private final AtomicInteger nextId = new AtomicInteger(1);
 
     private final Map<Integer, Vacancy> vacancies = new ConcurrentHashMap<>();
